f2703e0f59efa7232b9e6059e1a0c2f7107aa1a6
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-22 17:08:14 +0200

    Add index method for admin to print projects table

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+
+@section('content')
+    <section class="py-5">
+        <div class="container">
+            <h2 class="text-muted">All Projects</h2>
+            <div class="table-responsive">
+                <table class="table table-light table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @forelse ($projects as $project)
+                            <tr class="">
+                                <td scope="row">{{ $project->id }}</td>
+                                <td>
+                                    <img width="140" src="{{ $project->image }}" alt="{{ $project->title }}">
+                                </td>
+                                <td>{{ $project->title }}</td>
+                                <td>{{ $project->slug }}</td>
+                                <td>View/Edit/Delete</td>
+                            </tr>
+                        @empty
+
+                            <tr class="">
+                                <td scope="row" colspan="5">Under construction</td>
+                            </tr>
+                        @endforelse
+
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </section>
+@endsection
